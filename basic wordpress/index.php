892dbba9dbcede831979517d10f4c70941730303
Md. Foysal Ahammed <md.foysalahammedforhad@gmail.com>
2023-03-31 10:02:30 -0700

    Add files via upload

---
+++ basic wordpress/index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html <?php language_attributes(); ?> >
+<head>
+    <meta charset=" <?php bloginfo('charset') ?>">
+    <title> <?php bloginfo('name'); ?> </title>
+</head>
+<body <?php body_class(); ?>>
+    <h1> <a href=" <?php echo home_url(); ?> "> <?php bloginfo('name'); ?> </a></h1>
+    <h3> <?php bloginfo('description'); ?> </h3>
+
+    <!-- post Section -->
+    <?php 
+        if( have_posts()) :
+            while(have_posts()) : the_post(); ?>
+                
+                <h2> <a href=" <?php the_permalink(); ?> "> <?php the_title(); ?> </a> </h2>
+                <p> <?php the_content(); ?> </p>
+
+        <?php endwhile;
+
+        else :
+            echo 'Sorry, nothing found';
+        endif;
+
+    ?>
+
+</body>
+</html>
